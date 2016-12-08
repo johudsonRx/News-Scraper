1b7f16e1523204ae2bca80b3cd79143dc340c59a
Joshua Hudson <joshudson93@gmail.com>
2016-12-08 18:31:18 -0500

    Working on making it scrape

---
+++ server.js
@@ -0,0 +1,129 @@
+// Dependencies
+var request = require("request");
+var logger = require("morgan");
+var cheerio = require("cheerio");
+var express = require("express");
+var expressHandlebars = require("express-handlebars");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+
+// Pulling from our models file
+var Article = require("./models/article.js");
+
+// Database configuration
+// var mongojs = require('mongojs');
+// var databaseUrl = "scraper";
+// var collections = ["scrapedData"];
+
+// Hook mongojs configuration to the db variable
+// var db = mongojs(databaseUrl, collections);
+// db.on("error", function(error) {
+//   console.log("Database Error:", error);
+// });
+
+// Mongoose mpromise deprecated - use bluebird promises
+var Promise = require("bluebird");
+mongoose.Promise = Promise;
+
+// Initialize Express
+var app = express();
+
+// Use morgan and body parser with our app
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// Make public a static dir
+app.use(express.static("public"));
+
+// Database configuration with mongoose
+mongoose.connect("mongodb://localhost/filescraperdb");
+// Storing the connection in the db variable
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+
+// A GET request to scrape the techcrunch website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with request
+  request("https://techcrunch.com/startups/", function(error, response, html) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(html);
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article h2").each(function(i, element) {
+
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this).children("a").text();
+      result.link = $(this).children("a").attr("href");
+
+      // Using our Article model, create a new entry
+      // This effectively passes the result object to the entry (and the title and link)
+      var entry = new Article(result);
+
+      // Now, save that entry to the db
+      entry.save(function(err, doc) {
+        // Log any errors
+        if (err) {
+          console.log(err);
+        }
+        // Or log the doc
+        else {
+          console.log(doc);
+        }
+      });
+
+    });
+  });
+  // Tell the browser that we finished scraping the text
+  res.send("Scrape Complete");
+});
+
+app.get("/articles", function(req, res) {
+  // Grab every doc in the Articles array
+  Article.find({}, function(error, doc) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    // Or send the doc to the browser as a json object
+    else {
+      res.json(doc);
+    }
+  });
+});
+
+// app.get("/articles/:id", function(req, res) {
+//   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+//   Article.findOne({ "_id": req.params.id })
+//   // ..and populate all of the notes associated with it
+//   .populate("note")
+//   // now, execute our query
+//   .exec(function(error, doc) {
+//     // Log any errors
+//     if (error) {
+//       console.log(error);
+//     }
+//     // Otherwise, send the doc to the browser as a json object
+//     else {
+//       res.json(doc);
+//     }
+//   });
+// });
+
+
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
